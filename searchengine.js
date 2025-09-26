9cdde43f254955751e5cc6065e50c72c6efdd41a
Alexander DePue <apdepue@outlook.com>
2025-09-26 12:45:56 -0400

    added API key

---
+++ searchengine.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+const apiKey = "YOUR_API_KEY"; 
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+const gifContainer = document.getElementById('gif-container');
